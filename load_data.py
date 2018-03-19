a5a146fb794781c00a8e85714d9405a4de845e8f
Tarasko <taras.koroljuk@gmail.com>
2018-03-19 09:21:49 +0200

    wip

--- load_data.py
+++ load_data.py
@@ -33,7 +33,7 @@ faces = \
     Face.pack(1, 3, 2) + \
     Face.pack(4, 1, 0)
 
-face_groups = pack('9i', 0, 1, 0, 1, 2, 0, 1, 1, 2)
+face_groups = pack('9i', 0, 2, 0, 1, 2, 0, 1, 1, 2)
 
 offset = 0
 memory.write(hdr)
